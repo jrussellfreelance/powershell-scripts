5c0ab269d1712234e37986db0e82caf18cd7a89e
Jesse Russell <jesrus@mpicorp.com>
2018-01-01 06:14:12 -0500

    Added two scripts

---
+++ create-csv-from-mysql-table.ps1
@@ -0,0 +1,23 @@
+﻿$csvfilepath = "$PSScriptRoot\mysql_table.csv"
+Do {
+	$server = Read-Host "Please enter the server host name or IP of the database"
+}
+While ($server  -eq "")
+Do {
+	$database = Read-Host "Please enter the database name"
+}
+While ($database  -eq "")
+Do {
+	$dbuser = Read-Host "Please enter the database user"
+}
+While ($dbuser  -eq "")
+Do {
+	$dbpass = Read-Host "Please enter the database user's password"
+}
+While ($dbpass  -eq "")
+Do {
+	$query = Read-Host "Please enter the sql query"
+}
+While ($query  -eq "")
+$result = Invoke-MySqlQuery  -ConnectionString "server=$server; database=$database; user=$dbuser; password=$dbpass; pooling = false; convert zero datetime=True" -Sql $query -CommandTimeout 10000
+$result | Export-Csv $csvfilepath -NoTypeInformation
---
+++ create-csv-from-sqlserver-table.ps1
@@ -0,0 +1,15 @@
+﻿$csvfilepath = "$PSScriptRoot\sqlserver_table.csv"
+Do {
+	$server = Read-Host "Please enter the server host name or IP of the database"
+}
+While ($server  -eq "")
+Do {
+	$database = Read-Host "Please enter the database name"
+}
+While ($database  -eq "")
+Do {
+	$query = Read-Host "Please enter the sql query"
+}
+While ($query  -eq "")
+$result = Invoke-SqlServerQuery -Credential (Get-Credential) -ConnectionTimeout 10000 -Database $database -Server $server -Sql $query -CommandTimeout 10000
+$result | Export-Csv $csvfilepath -NoTypeInformation
