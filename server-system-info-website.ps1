ffbd31734cb048c5a8efd9528bffb504e0eb5ff7
Jesse Russell <jesrus@mpicorp.com>
2018-01-03 17:42:31 -0500

    Totally revamped sys info report scripts

--- server-system-info-website.ps1
+++ server-system-info-website.ps1
@@ -3,6 +3,7 @@
 $servers = Get-Content "servers.txt"
 # Specify the path to the webroot here
 $webroot = "" # example: \\webserver\c$\inetpub\wwwroot
+$indexpath = "$webroot\index.html"
 # Loop through servers
 foreach ($server in $servers) {
 # Create file path
@@ -14,18 +15,34 @@ $boot = Get-WmiObject -ComputerName $server Win32_OperatingSystem | Select-Objec
 $os = Get-WmiObject -ComputerName $server Win32_OperatingSystem | Select -ExpandProperty Caption
 $arch = Get-WmiObject -ComputerName $server Win32_OperatingSystem | Select -ExpandProperty OSArchitecture
 $processor = Get-WmiObject -ComputerName $server Win32_Processor | Select -ExpandProperty Name
+$free = [math]::Round((Get-WmiObject -ComputerName $server Win32_OperatingSystem | Select -ExpandProperty FreePhysicalMemory)/1MB, 2)
+$numProcesses = Get-WmiObject -ComputerName $server Win32_OperatingSystem | Select -ExpandProperty NumberOfProcesses
+$numUsers = Get-WmiObject -ComputerName $server Win32_OperatingSystem | Select -ExpandProperty NumberOfUsers
+$model = Get-WmiObject -ComputerName $server win32_ComputerSystemProduct | Select -ExpandProperty Name
+$manufacturer = Get-WmiObject -ComputerName $server win32_ComputerSystemProduct | Select -ExpandProperty Vendor
 
 # Create computer object and html
 $computerProps = @{
 	'Name'= $name;
 	'Operating System'= $os;
 	'Architecture'= $arch;
-	'Memory (GB)'= $ram;
 	'Processor'= $processor;
-	'Last Boot'= $boot;
+    'Model Name'=$model;
+    'Manufacturer'=$manufacturer;
+	'Last Boot'= $boot
 }
 $computer = New-Object -TypeName PSObject -Prop $computerProps
 $computerHtml = $computer | ConvertTo-Html -Fragment -PreContent "<h1>$server Report</h1><h2>System Information</h2>"
+
+# Create computer object and html
+$perfmonProps = @{
+	'Free Memory (GB)'= $free;
+	'Total Memory (GB)'= $ram;
+    'Process Count'=$numProcesses;
+    'User Count'=$numUsers
+}
+$perfmon = New-Object -TypeName PSObject -Prop $perfmonProps
+$perfmonHtml = $perfmon | ConvertTo-Html -Fragment -PreContent "<h2>Performance Information</h2>"
 
 # List network stats
 $ips = Get-WmiObject -ComputerName $server Win32_NetworkAdapterConfiguration | Where-Object { $_.IPAddress -ne $null } | Select-Object DNSHostName,Description,@{Label="IP Address"; Expression={($_.IPAddress[0])}},@{Label="Default Gateway"; Expression={($_.DefaultIPGateway[0])}},MACAddress
@@ -36,21 +53,33 @@ $disks = Get-WmiObject -ComputerName $server Win32_LogicalDisk -Filter "DriveTyp
 $disksHtml = $disks | ConvertTo-Html -Fragment -PreContent "<h2>Drives</h2>"
 
 # Select all processes that are over 50 MB
-$processes = Get-Process -ComputerName $server | Where-Object {$_.WorkingSet -gt 52428800} | Sort-Object WorkingSet -Descending | Select-Object @{Label="Process"; Expression={$_.ProcessName}}, @{Label="Memory Usage (MB)"; Expression={“{0:N2}” -f ($_.WorkingSet / 1MB)}}
+$processes = Get-WmiObject -ComputerName $server Win32_Process | Where-Object {$_.WorkingSetSize -gt 52428800} | Sort-Object WorkingSetSize -Descending | Select-Object Name,ProcessId,@{Label="Memory Usage (MB)"; Expression={“{0:N2}” -f ($_.WorkingSetSize / 1MB)}}
 $processesHtml = $processes | ConvertTo-Html -Fragment -PreContent "<h2>Runnning Processes over 50MB</h2>"
 
 # Select all running services
-$services= Get-Service -ComputerName $server | Where-Object {$_.Status -eq "Running"} | Sort-Object DisplayName | Select-Object @{Label="Service"; Expression={$_.DisplayName}}
+$services= Get-WmiObject -ComputerName $server Win32_Service | Where-Object {$_.State -eq "Running"} | Sort-Object DisplayName | Select-Object DisplayName,ProcessId,StartMode
 $servicesHtml = $services | ConvertTo-Html -Fragment -PreContent "<h2>Running Services</h2>"
+
+# Select all start up programs
+$startup= Get-WmiObject -ComputerName $server Win32_startupCommand | Sort-Object Caption | Select-Object Caption,User,Command
+$startupHtml = $startup | ConvertTo-Html -Fragment -PreContent "<h2>Startup Commands</h2>"
 
 # Create HTML file
 $head = @"
-	<title>Computer Report</title>
+	<title>Server Report</title>
 	<style>
 		body {
 			background-color: #FFFFFF;
 			font-family: sans-serif;
 		}
+
+        a {
+            color: #666666;
+        }
+
+        a:hover {
+            color: #1E87F0;
+        }
 
 		h1 {
 			color: #1E87F0;
@@ -80,6 +109,15 @@ $head = @"
 "@
 
 # Convert everything to HTML and output to file
-ConvertTo-Html -Head $head -Body "$computerHtml $ipsHtml $disksHtml $processesHtml $servicesHtml" | Out-File $filepath
-
+ConvertTo-Html -Head $head -Body "$computerHtml $perfmonHtml $ipsHtml $disksHtml $processesHtml $servicesHtml $startupHtml" | Out-File $filepath
 }
+
+$indexHtml = ""
+$indexHtml += "<h1>Server Health Report</h1>"
+$indexHtml += "<table><tr><th>Server Name</th></tr><tr>"
+foreach ($server in $servers) {
+    $indexHtml += "<td><a href='/$server'>$server</a></td>"
+}
+$indexHtml += "</tr></table>"
+
+ConvertTo-Html -Head $head -Body $indexHtml | Out-File $indexpath
--- system-info-html-report-local.ps1
+++ system-info-html-report-local.ps1
@@ -4,23 +4,39 @@ $reportPath = $PSScriptRoot + "\report.html"
 
 # Computer info
 $ram = [math]::Round((Get-WmiObject Win32_OperatingSystem | Select -ExpandProperty TotalVisibleMemorySize)/1MB, 2)
+$free = [math]::Round((Get-WmiObject Win32_OperatingSystem | Select -ExpandProperty FreePhysicalMemory)/1MB, 2)
 $name = Get-WmiObject Win32_ComputerSystem | Select -ExpandProperty Name
+$numProcesses = Get-WmiObject Win32_OperatingSystem | Select -ExpandProperty NumberOfProcesses
+$numUsers = Get-WmiObject Win32_OperatingSystem | Select -ExpandProperty NumberOfUsers
 $boot = Get-WmiObject Win32_OperatingSystem | Select-Object @{Label="LastBootTime"; Expression={$_.ConvertToDateTime($_.LastBootUpTime)}} | Select -ExpandProperty LastBootTime
 $os = Get-WmiObject Win32_OperatingSystem | Select -ExpandProperty Caption
 $arch = Get-WmiObject Win32_OperatingSystem | Select -ExpandProperty OSArchitecture
 $processor = Get-WmiObject Win32_Processor | Select -ExpandProperty Name
+$model = Get-WmiObject win32_ComputerSystemProduct | Select -ExpandProperty Name
+$manufacturer = Get-WmiObject win32_ComputerSystemProduct | Select -ExpandProperty Vendor
 
 # Create computer object and html
 $computerProps = @{
 	'Name'= $name;
 	'Operating System'= $os;
 	'Architecture'= $arch;
-	'Memory (GB)'= $ram;
 	'Processor'= $processor;
-	'Last Boot'= $boot;
+    'Model Name'=$model;
+    'Manufacturer'=$manufacturer;
+	'Last Boot'= $boot
 }
 $computer = New-Object -TypeName PSObject -Prop $computerProps
 $computerHtml = $computer | ConvertTo-Html -Fragment -PreContent "<h1>Computer Report</h1><h2>System Information</h2>"
+
+# Create computer object and html
+$perfmonProps = @{
+	'Free Memory (GB)'= $free;
+	'Total Memory (GB)'= $ram;
+    'Process Count'=$numProcesses;
+    'User Count'=$numUsers
+}
+$perfmon = New-Object -TypeName PSObject -Prop $perfmonProps
+$perfmonHtml = $perfmon | ConvertTo-Html -Fragment -PreContent "<h2>Performance Information</h2>"
 
 # List network stats
 $ips = Get-WmiObject Win32_NetworkAdapterConfiguration | Where-Object { $_.IPAddress -ne $null } | Select-Object DNSHostName,Description,@{Label="IP Address"; Expression={($_.IPAddress[0])}},@{Label="Default Gateway"; Expression={($_.DefaultIPGateway[0])}},MACAddress
@@ -31,12 +47,16 @@ $disks = Get-WmiObject Win32_LogicalDisk -Filter "DriveType=3" | Select-Object @
 $disksHtml = $disks | ConvertTo-Html -Fragment -PreContent "<h2>Drives</h2>"
 
 # Select all processes that are over 50 MB
-$processes = Get-Process | Where-Object {$_.WorkingSet -gt 52428800} | Sort-Object WorkingSet -Descending | Select-Object @{Label="Process"; Expression={$_.ProcessName}}, @{Label="Memory Usage (MB)"; Expression={“{0:N2}” -f ($_.WorkingSet / 1MB)}}
+$processes = Get-WmiObject Win32_Process | Where-Object {$_.WorkingSetSize -gt 52428800} | Sort-Object WorkingSetSize -Descending | Select-Object Name,ProcessId,@{Label="Memory Usage (MB)"; Expression={“{0:N2}” -f ($_.WorkingSetSize / 1MB)}}
 $processesHtml = $processes | ConvertTo-Html -Fragment -PreContent "<h2>Runnning Processes over 50MB</h2>"
 
 # Select all running services
-$services= Get-Service | Where-Object {$_.Status -eq "Running"} | Sort-Object DisplayName | Select-Object @{Label="Service"; Expression={$_.DisplayName}}
+$services= Get-WmiObject Win32_Service | Where-Object {$_.State -eq "Running"} | Sort-Object DisplayName | Select-Object DisplayName,ProcessId,StartMode
 $servicesHtml = $services | ConvertTo-Html -Fragment -PreContent "<h2>Running Services</h2>"
+
+# Select all start up programs
+$startup= Get-WmiObject Win32_startupCommand | Sort-Object Caption | Select-Object Caption,User,Command
+$startupHtml = $startup | ConvertTo-Html -Fragment -PreContent "<h2>Startup Commands</h2>"
 
 # Create HTML file
 $head = @"
@@ -75,4 +95,4 @@ $head = @"
 "@
 
 # Convert everything to HTML and output to file
-ConvertTo-Html -Head $head -Body "$computerHtml $ipsHtml $disksHtml $processesHtml $servicesHtml" | Out-File $reportPath
+ConvertTo-Html -Head $head -Body "$computerHtml $perfmonHtml $ipsHtml $disksHtml $processesHtml $servicesHtml $startupHtml" | Out-File $reportPath
