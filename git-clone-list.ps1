30154948b34078697846cd7151fa766c2985f32b
Jesse Russell <jesse@jrussell.io>
2019-02-21 08:12:07 -0800

    Added git-config-list

---
+++ git-clone-list.ps1
@@ -0,0 +1,6 @@
+﻿# This script clones a list of repos from a text file
+# repos.txt should just be a list of .git URLs, separated by a carriage return
+$repos = Get-Content "$PSScriptRoot\repos.txt"
+foreach ($repo in $repos) {
+    git clone $repo
+}
